18b0e26c9ad70f173775dfdd7a604c8f54142515
patrick <patrick.laranjo@gmail.com>
2019-01-29 12:29:42 +0100

    code

--- src/h02/Opdracht201.java
+++ src/h02/Opdracht201.java
@@ -1,4 +1,16 @@
 package h02;
 
-public class Opdracht201 {
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht201 extends Applet {
+
+    public void init(){
+        setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g){
+        g.setColor(Color.yellow);
+        g.drawString("Patrick", 50, 50);
+    }
 }
