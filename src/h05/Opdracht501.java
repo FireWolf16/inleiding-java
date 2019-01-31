690d103ba72f4ce4bb51079cd1552c4ce7fa624c
patrick <patrick.laranjo@gmail.com>
2019-01-31 13:59:49 +0100

    code

--- src/h05/Opdracht501.java
+++ src/h05/Opdracht501.java
@@ -21,21 +21,29 @@ public class Opdracht501 extends Applet {
 
     @Override
     public void paint(Graphics g) {
-        //teken rechthoek
-        g.drawRect(100, 100, breedte, hoogte);
-        //teken afgeronde rechthoek
-        g.drawRoundRect(100, 225, breedte, hoogte, 30, 30);
-        //teken gevulde rechthoek
+        //begin Lijn//
+        g.drawLine(20,100,breedte,hoogte);
+        //begin Rechthoek
+        g.drawRect(20,200,breedte,hoogte);
+        //begin gevulde rechthoek met ovaal
         g.setColor(opvulkleur);
-        g.fillRect(315, 100, breedte, hoogte);
-        //teken ovaal
+        g.fillRect(240,200,breedte,hoogte);
         g.setColor(lijnkleur);
-        g.drawOval(315, 100, breedte, hoogte);
-        //teken gevulde ovaal
+        g.drawArc(240, 200, breedte, hoogte, 90, 360);
+        //begin Taartpunt met ovaal eromheen
+        g.drawArc(460, 200, breedte, hoogte, 90, 360);
         g.setColor(opvulkleur);
-        g.fillOval(315, 225, breedte, hoogte);
-        // en zo voort...
-
+        g.fillArc(460,200,breedte,hoogte,0,45);
+        //begin afgeronde rechthoek
+        g.setColor(lijnkleur);
+        g.drawRoundRect(20,340,breedte,hoogte,30,30);
+        //begin gevulde ovaall
+        g.setColor(opvulkleur);
+        g.fillArc(240, 340, breedte, hoogte, 90, 360);
+        //begin cirkel
+        breedte = 100;
+        g.setColor(lijnkleur);
+        g.drawArc(510,340,breedte,hoogte,0,360);
     }
 
 
