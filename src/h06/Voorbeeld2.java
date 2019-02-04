dc8779ed8b73fecbae65f2c09e54fcbcf6e0a4f9
patrick <patrick.laranjo@gmail.com>
2019-02-04 09:45:03 +0100

    code

---
+++ src/h06/Voorbeeld2.java
@@ -0,0 +1,20 @@
+package h06;
+// Voorbeeld 6.2
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class Voorbeeld2 extends Applet {
+    int teller;
+
+    public void init() {
+        teller = 0;
+    }
+
+    public void paint(Graphics g) {
+        teller++;
+        g.drawString("Scherm is " + teller +
+                " keer ververst.", 50, 60 );
+    }
+}
